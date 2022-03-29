025a9d76e177b1d1fa80d5831a83e690f34d6773
Casper Rysgaard <casperrysgaard@gmail.com>
2022-03-29 14:10:05 +0200

    Make tree constructor function used for making queries

--- Main.hs
+++ Main.hs
@@ -5,11 +5,12 @@ import qualified Data.Map.Strict as MB
 import DataRecords as D
 import DAG_construction as C
 import Query as Q
+import Tree_Constructor as TC
 
 import Debug.Trace
 
-stupidContains :: Int -> Int -> MB.Map Int (FrozenNode Int) -> Bool
-stupidContains key time rootMap =
+stupidContains :: MB.Map Int (FrozenNode Int) -> Int -> Int -> Bool
+stupidContains rootMap time key =
     let func info fieldQuery = case info of
             Nothing -> False
             Just value -> 
@@ -19,7 +20,16 @@ stupidContains key time rootMap =
     
     query func rootMap time
 
-main =
+stupidContains2 :: (Int -> Tree Int) -> Int -> Int -> Bool
+stupidContains2 treeGen time key =
+    let contains tree = case tree of
+            Leaf -> False
+            Node {elm=elm, children=children} -> 
+                elm == key || (any contains children)
+    in
+    contains (treeGen time)
+
+main = do
     let graph = C.build
                 [ D.TimeEdge {id_from = 0, field = 0, id_to = 1, time_from = 2, time_to = 4}
                 , D.TimeEdge {id_from = 1, field = 1, id_to = 2, time_from = 1, time_to = 2}
@@ -30,6 +40,6 @@ main =
                 [(0, 0 :: Int), (1, 1 :: Int), (2, 2 :: Int), (3, 3 :: Int), (4, 4 :: Int)]
                 [(1, 1), (2, 0)]
                 5 2
-    in
     -- print (MB.lookupLE 4 graph)
-    print (stupidContains 3 3 graph)
+    print (stupidContains graph 3 3)
+    print (stupidContains2 (construct graph) 3 1)
--- Tree_Constructor.hs
+++ Tree_Constructor.hs
@@ -1,19 +1,38 @@
 
-module Construct where
+module Tree_Constructor where
 
 import DataRecords as D
 
 import qualified Data.List as L
 import qualified Data.Map.Strict as MB
 
--- construct :: MB.Map Int (FrozenNode s) -> Int -> Tree s
--- construct rootMap time =
---     let root = case MB.lookupLE time rootMap of
---             Just (_, root) -> root
---             Nothing -> error ("No root exists at time " ++ (show time) ++ "!")
---     in
-    
---     let inner frozenTree
---             = case frozenTree of
---                 FrozenLeaf -> Leaf
+import Debug.Trace
 
+construct :: Show s => MB.Map Int (FrozenNode s) -> Int -> Tree s
+construct rootMap time =
+    let root = case MB.lookupLE time rootMap of
+            Nothing -> Nothing
+            Just (_, root) -> Just root
+    in
+    
+    let gen frozenTree
+            = case frozenTree of
+                Nothing -> Leaf
+                Just (FrozenNode {staticInformation=staticInformation, fields=fields}) ->
+                    -- trace ("Generating node " ++ (show staticInformation) ++ " ...") $
+                    let children
+                            = map (\edgeList ->
+                                    let validEdges = filter (\e -> frozen_time_from e <= time && time < frozen_time_to e) edgeList in
+                                    case validEdges of
+                                        [] -> gen Nothing
+                                        [FrozenEdge {node_to=node_to}] -> gen (Just node_to)
+                                        _ -> error "Multiple valid edges at time!"
+                            ) fields
+                    in
+                    Node {
+                        elm = staticInformation,
+                        children = children
+                    }
+    in
+    
+    gen root
